57d92ffd1ddb95af3cc22afa2acbe9fba544ab95
haliljon <juraboevhaliljon@gmail.com>
2023-11-03 01:39:09 +0500

    Fix cors error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'http://localhost:3000'
+        origins 'https://usercontrol-pro.netlify.app/'
         resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
     end
 
