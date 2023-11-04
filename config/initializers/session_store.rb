e44da2e2752bd45c7a12bcef5240f244c0fa8d7d
haliljon <juraboevhaliljon@gmail.com>
2023-11-04 16:03:25 +0500

    Add domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'https://authentication-app-frontend.herokuapp.com'
+    Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'https://user-control-pro-6d69dacacf7c.herokuapp.com/'
 else
     Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
